79fcc69247cf6ee0748ea5b6588e7917f1b6d7aa
DrymarchonShaun <40149778+drymarchonshaun@users.noreply.github.com>
2026-07-27 14:50:32 -0700

    python-unrar: disable python metadata check

--- pkgs/development/python-modules/python-unrar/default.nix
+++ pkgs/development/python-modules/python-unrar/default.nix
@@ -1,46 +1,45 @@
 {
-  stdenv,
   lib,
-  replaceVars,
-  buildPythonPackage,
   fetchPypi,
-  unrar,
-  pytestCheckHook,
+  buildPythonPackage,
+  nix-update-script,
   setuptools,
+  unrar,
 }:
-buildPythonPackage rec {
+
+buildPythonPackage (finalAttrs: {
   pname = "python-unrar";
   version = "0.4";
   pyproject = true;
+  __structuredAttrs = true;
 
   src = fetchPypi {
     pname = "unrar";
-    inherit version;
+    inherit (finalAttrs) version;
     hash = "sha256-skRHpbkwJL5gDvglVmi6I6MPRRF2V3tpFVnqE1n30WQ=";
   };
+
+  postPatch = ''
+    substituteInPlace unrar/unrarlib.py \
+      --replace-fail "os.environ.get('UNRAR_LIB_PATH', None)" "${unrar}/lib/libunrar.so"
+  '';
 
   build-system = [
     setuptools
   ];
 
-  patches = [
-    (replaceVars ./use_nix_unrar_path.patch {
-      unrar_lib_path = "${unrar}/lib/libunrar${stdenv.hostPlatform.extensions.sharedLibrary}";
-    })
+  pythonImportsCheck = [
+    "unrar"
   ];
 
-  nativeCheckInputs = [ pytestCheckHook ];
+  dontCheckPythonMetadata = true;
 
-  doCheck = true;
-
-  pythonImportsCheck = [ "unrar" ];
+  passthru.updateScript = nix-update-script { };
 
   meta = {
-    homepage = "http://github.com/matiasb/python-unrar";
-    changelog = "https://github.com/matiasb/python-unrar/releases/tag/v${version}";
-    description = "Wrapper for UnRAR library, plus a rarfile module on top of it";
-    license = lib.licenses.gpl3Plus;
+    description = "Wrapper for UnRAR library, ctypes-based";
+    homepage = "https://pypi.org/project/unrar";
+    license = lib.licenses.gpl3Only;
     maintainers = with lib.maintainers; [ DrymarchonShaun ];
-    platforms = lib.platforms.linux;
   };
-}
+})
